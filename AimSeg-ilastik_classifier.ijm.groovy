67e76aaf46beb2ffc0af1f866c4abb967b650e25
Pau Carrillo-Barbera <pau.carrillob@gmail.com>
2022-06-03 20:55:19 +0100

    import probabilities

--- AimSeg-ilastik_classifier.ijm.groovy
+++ AimSeg-ilastik_classifier.ijm.groovy
@@ -1,16 +1,35 @@
-#@ File(label="Image File", style="open") file
+#@ File(label="Image File", style="open") imageFile
 
 import ij.IJ
 import ij.io.Opener
 import ij.ImagePlus
+import groovy.io.FileType
 import ij.gui.WaitForUserDialog
 
 // setup
-importImage(file)
 installMacro()
+imp = importImage(imageFile)
+
+// import the corresponding probability map
+// IMPORTANT: must be in the parent folder
+File parentPath = imageFile.getParentFile()
+def fileList = []
+parentPath.eachFile(FileType.FILES) {
+	fileList << it.name
+}
+String probNameWithoutExtension = imageFile.name.take(imageFile.name.lastIndexOf('.'))+"_Probabilities"
+String probFilename = fileList.find {element -> element.contains(probNameWithoutExtension)}
+File probFile = new File (parentPath, probFilename)
+impProb = importImage(probFile)
+//impProb.hide()
 
 def wfu = new WaitForUserDialog("Title", "I'm waiting")
 wfu.show()
+
+
+
+
+
 
 def installMacro(){
 	String toolsetsPath = IJ.getDir("macros") + "toolsets"
@@ -23,11 +42,13 @@ def importImage(inputFile){
 	if (!imagePath.endsWith(".h5")) {		
 		def opener = new Opener()
 		println "Opening file"
-		imp = opener.openImage(imagePath)
-		imp.show()
+		implus = opener.openImage(imagePath)
+		implus.show()
 	} else {
 		args = "select=[" + imagePath + "] datasetname=/data axisorder=tzyxc"
 		println "Opening h5 file"
 		IJ.run("Import HDF5", args)
+		implus = IJ.getImage()
 	}
+	return implus
 }
