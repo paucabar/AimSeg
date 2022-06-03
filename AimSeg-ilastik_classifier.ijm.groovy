79f2a947ef4b3edf40f9699448d42ab82d6fd6f9
Pau Carrillo-Barbera <pau.carrillob@gmail.com>
2022-06-03 21:37:58 +0100

    get file extension when opening

--- AimSeg-ilastik_classifier.ijm.groovy
+++ AimSeg-ilastik_classifier.ijm.groovy
@@ -8,7 +8,7 @@ import ij.gui.WaitForUserDialog
 
 // setup
 installMacro()
-imp = importImage(imageFile)
+imp = importImage(imageFile, "/data", "tzyxc")
 
 // import the corresponding probability map
 // IMPORTANT: must be in the parent folder
@@ -20,9 +20,10 @@ parentPath.eachFile(FileType.FILES) {
 String probNameWithoutExtension = imageFile.name.take(imageFile.name.lastIndexOf('.'))+"_Probabilities"
 String probFilename = fileList.find {element -> element.contains(probNameWithoutExtension)}
 File probFile = new File (parentPath, probFilename)
-impProb = importImage(probFile)
+impProb = importImage(probFile, "/exported_data", "yxc")
 //impProb.hide()
 
+// wait for user
 def wfu = new WaitForUserDialog("Title", "I'm waiting")
 wfu.show()
 
@@ -37,16 +38,17 @@ def installMacro(){
 	IJ.run("Install...", "install=[${->ijmPath}]")
 }
 
-def importImage(inputFile){
+def importImage(File inputFile, String datasetname, String axisorder){
 	String imagePath = inputFile.getAbsolutePath()
 	if (!imagePath.endsWith(".h5")) {		
 		def opener = new Opener()
-		println "Opening file"
+		String extension = imagePath[imagePath.lastIndexOf('.')+1..-1]
+		println "Importing $extension file"
 		implus = opener.openImage(imagePath)
 		implus.show()
 	} else {
-		args = "select=[" + imagePath + "] datasetname=/data axisorder=tzyxc"
-		println "Opening h5 file"
+		args = "select=[" + imagePath + "] datasetname=$datasetname axisorder=$axisorder"
+		println "Importing h5 file"
 		IJ.run("Import HDF5", args)
 		implus = IJ.getImage()
 	}
