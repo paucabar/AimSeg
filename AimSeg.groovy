6e1e4c4a8e066376491c52fc7dbadbdb9e1ef587
Pau Carrillo-Barbera <pau.carrillob@gmail.com>
2022-06-30 19:03:45 +0100

    define transferProperties and convexHull functions

--- AimSeg.groovy
+++ AimSeg.groovy
@@ -186,6 +186,34 @@ ImagePlus runMarkerControlledWatershed(ImageProcessor input, ImageProcessor labe
 	return impResult
 }
 
+private static void transferProperties(Roi roi1, Roi roi2) {
+	if (roi1==null || roi2==null)
+		return;
+	roi2.setStrokeColor(roi1.getStrokeColor());
+	if (roi1.getStroke()!=null)
+		roi2.setStroke(roi1.getStroke());
+	roi2.setDrawOffset(roi1.getDrawOffset());
+	if (roi1.getGroup()!=null)
+		roi2.setGroup(roi1.getGroup());
+}
+
+private static void convexHull(ImagePlus imp, RoiManager rm) {
+	int roiCount = rm.getCount()
+	println "$roiCount selected ROIs"
+	for (i in 0..roiCount-1) {
+		rm.select(0)
+		Roi roi = imp.getRoi()
+		FloatPolygon p = roi.getFloatConvexHull();
+			if (p!=null) {
+				Roi roi2 = new PolygonRoi(p, Roi.POLYGON);
+				transferProperties(roi, roi2);
+				rm.addRoi(roi2)
+				rm.select(0)
+				rm.runCommand(imp,"Delete")
+			}
+	}
+}
+
 
 
 //////////////
@@ -489,3 +517,4 @@ cmd.closeAll()
 // reset Prefs.padEdges
 Prefs.padEdges = pe
 Prefs.blackBackground = bb
+return
