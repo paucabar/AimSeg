03120f855303960d70f452a0349a1a793e9e6210
Pau Carrillo-Barbera <pau.carrillob@gmail.com>
2022-08-03 12:30:43 +0100

    define preProcessing as a function

--- Pre-processing_.groovy
+++ Pre-processing_.groovy
@@ -1,13 +1,16 @@
-#@ImagePlus imp
+#@ ImagePlus imp
 
 import ij.IJ
 import ij.process.ImageConverter
 import ij.ImagePlus
 
-int bitDepth = imp.getBitDepth()
-if (bitDepth > 8) {
-	def ic = new ImageConverter(imp)
-	ic.convertToGray8()
+def preProcessing(ImagePlus imp, float saturatedPixels) {
+	int bitDepth = imp.getBitDepth()
+	if (bitDepth > 8) {
+		def ic = new ImageConverter(imp)
+		ic.convertToGray8()
+	}
+	IJ.run(imp, "Enhance Contrast...", "saturated=$saturatedPixels update")
 }
-float saturatedPixels = 0.3
-IJ.run(imp, "Enhance Contrast...", "saturated=$saturatedPixels update")
+
+preProcessing(imp, 0.3)
