ade7a5d40e3919e5ccceb2fd2a7053888db1cdec
Pau Carrillo-Barbera <pau.carrillob@gmail.com>
2023-06-19 13:24:18 +0200

    reorganize AimSeg

---
+++ source/AimSeg/Export_Masks.groovy
@@ -0,0 +1,115 @@
+#@ File(label="Image File", style="open") imageFile
+
+import java.io.File
+import ij.IJ
+import ij.io.Opener
+import ij.io.FileSaver
+import ij.ImagePlus
+import org.ilastik.ilastik4ij.hdf5.Hdf5DataSetReader
+import ij.plugin.frame.RoiManager
+//import ij.gui.Overlay
+import ij.plugin.filter.ThresholdToSelection
+import ij.process.ImageProcessor
+import ij.gui.Roi
+//import java.awt.Color
+
+
+/**
+ * Opens an image file
+ * If the file extension is h5, the image is imported using the ilastik's importer
+ * Otherwise, the file is imported using ImageJ's opener
+ */
+ImagePlus importImage (File inputFile, String datasetName, String axisOrder) {
+    String imagePath = inputFile.getAbsolutePath()
+    if (!imagePath.endsWith(".h5")) {
+        def opener = new Opener()
+        String extension = imagePath[imagePath.lastIndexOf('.')+1..-1]
+        println "Importing $extension file"
+        result = opener.openUsingBioFormats(imagePath)
+    } else {
+        println "Importing h5 file"
+        def imp = new Hdf5DataSetReader<>(
+                imagePath,
+                datasetName,
+                axisOrder.toLowerCase(),
+                logService,
+                statusService).read()
+        result = ImageJFunctions.wrap(imp, "Some title here")
+    }
+    return result
+}
+
+ImagePlus label_semantic(ImagePlus impSemantic, int classIndex, RoiManager roiSet) {
+	ip = impSemantic.getProcessor()
+	roiSet.getRoisAsArray().each { roi ->
+		ip.setColor(classIndex)
+		ip.fill(roi)
+	}	
+	ip.setMinAndMax(0.0, impSemantic.getStatistics().max)
+	impSemantic = new ImagePlus("Semantic Labels", ip)
+	IJ.run(impSemantic, "glasbey on dark", "")
+	return impSemantic
+}
+
+ImagePlus label_instance(ImagePlus impInstance, RoiManager roiSet, String objectName) {
+	ip = impInstance.getProcessor()
+	roiSet.getRoisAsArray().each { roi ->
+		ip.setColor(roi.getName() as int)
+		ip.fill(roi)
+	}	
+	ip.setMinAndMax(0.0, impInstance.getStatistics().max)
+	impInstance = new ImagePlus("$objectName Labels", ip)
+	IJ.run(impInstance, "glasbey on dark", "")
+	return impInstance
+}
+
+// import EM image
+imp = importImage(imageFile, "/data", "tzyxc")
+
+// create 3 RoiManager
+RoiManager rmIn = new RoiManager(false)
+RoiManager rmOut = new RoiManager(false)
+RoiManager rmAxon = new RoiManager(false)
+
+String parentPathS = imageFile.getParentFile()
+String impNameWithoutExtension = imageFile.name.take(imageFile.name.lastIndexOf('.'))
+rmIn.open(parentPathS+File.separator+impNameWithoutExtension+"_RoiSet_IN.zip")
+rmOut.open(parentPathS+File.separator+impNameWithoutExtension+"_RoiSet_OUT.zip")
+rmAxon.open(parentPathS+File.separator+impNameWithoutExtension+"_RoiSet_AXON.zip")
+
+// create images to add labels
+ImagePlus impSemantic = IJ.createImage("Labeling", "16-bit black", imp.getWidth(), imp.getHeight(), 1)
+ImagePlus impFibre = IJ.createImage("Labeling", "16-bit black", imp.getWidth(), imp.getHeight(), 1)
+ImagePlus impInnerMyelin = IJ.createImage("Labeling", "16-bit black", imp.getWidth(), imp.getHeight(), 1)
+ImagePlus impAxon = IJ.createImage("Labeling", "16-bit black", imp.getWidth(), imp.getHeight(), 1)
+
+// draw semantic labels
+impSemantic = label_semantic (impSemantic, 1, rmOut)
+impSemantic = label_semantic (impSemantic, 2, rmIn)
+impSemantic = label_semantic (impSemantic, 3, rmAxon)
+
+// draw instances
+impFibre = label_instance(impFibre, rmOut, "Fibre")
+impInnerMyelin = label_instance(impInnerMyelin, rmIn, "Inner Myelin")
+impAxon = label_instance(impAxon, rmAxon, "Axon")
+
+// create directories
+File exportDir = new File(parentPathS+File.separator+"export")
+File semanticDir = new File(exportDir.getAbsolutePath()+File.separator+"semantic")
+File instanceDir = new File(exportDir.getAbsolutePath()+File.separator+"instance")
+File fibreDir = new File(instanceDir.getAbsolutePath()+File.separator+"fibre")
+File innerMyelinDir = new File(instanceDir.getAbsolutePath()+File.separator+"inner_myelin")
+File axonDir = new File(instanceDir.getAbsolutePath()+File.separator+"axon")
+exportDir.mkdir()
+semanticDir.mkdir()
+instanceDir.mkdir()
+fibreDir.mkdir()
+innerMyelinDir.mkdir()
+axonDir.mkdir()
+
+// save images
+new FileSaver(impSemantic).saveAsTiff(semanticDir.getAbsolutePath()+File.separator+impNameWithoutExtension+".tif")
+new FileSaver(impFibre).saveAsTiff(fibreDir.getAbsolutePath()+File.separator+impNameWithoutExtension+".tif")
+new FileSaver(impInnerMyelin).saveAsTiff(innerMyelinDir.getAbsolutePath()+File.separator+impNameWithoutExtension+".tif")
+new FileSaver(impAxon).saveAsTiff(axonDir.getAbsolutePath()+File.separator+impNameWithoutExtension+".tif")
+return
---
+++ source/AimSeg/Pre-processing_.groovy
@@ -0,0 +1,79 @@
+#@ File(label="Select directory", style="directory") dir
+#@ float(label="Downsample", value=2.0, persist=true) downsample
+#@ boolean(label="Normalize", value=false, persist=true) normalize
+#@ float(label="% Sat Pixels", value=1.0, persist=true) saturated
+
+import ij.IJ
+import ij.process.ImageConverter
+import ij.io.Opener
+import ij.ImagePlus
+import ij.plugin.ContrastEnhancer
+import ij.process.ImageProcessor
+import java.io.File
+import groovy.io.FileType
+
+ImagePlus importImage (File inputFile) {
+	String imagePath = inputFile.getAbsolutePath()
+	def opener = new Opener()
+	// Use DM3_Reader.java is available (and required)
+	opener.useHandleExtraFileTypes = true
+	String extension = imagePath[imagePath.lastIndexOf('.')+1..-1]
+	println "Importing $extension file"
+	result = opener.openUsingBioFormats(imagePath)
+	return result
+}
+
+ImagePlus resizeImage(ImagePlus imp, float downsample) {
+	ImagePlus result = imp.resize(Math.ceil(imp.getWidth()/downsample).intValue(), Math.ceil(imp.getHeight()/downsample).intValue(), "none")
+	return result
+}
+
+def preProcessing(ImagePlus imp, float saturated, float downsample) {
+	if (normalize) {
+		int bitDepth = imp.getBitDepth()
+		if (bitDepth != 32) {
+			ImageConverter ic = new ImageConverter(imp)
+			//ic.setDoScaling(true)
+			ic.convertToGray32()
+		}
+		ContrastEnhancer ce = new ContrastEnhancer()
+		ce.setNormalize(true)
+		ce.stretchHistogram(imp, saturated)
+		imp.getProcessor().resetMinAndMax()
+	}
+	if (downsample != 1.0) {
+		imp = resizeImage(imp, downsample)
+	}
+	//imp.show()
+	return imp
+}
+
+// create file list
+def fileList = []
+dir.eachFile(FileType.FILES) {
+	fileList << it.name
+}
+
+// create output folder
+File outDir = new File(dir.getParentFile(), dir.getName()+'_pre-processed')
+if (outDir.getParentFile() != null) {
+  outDir.mkdirs()
+}
+outDir.createNewFile()
+
+// import, process and save
+for (i=0; i<fileList.size(); i++) {
+	File file = new File (dir, fileList[i])
+	// Skip hidden files
+	if (file.isHidden())
+		continue
+	ImagePlus imp = importImage(file)
+	String title = imp.getShortTitle()
+	println "Processing ${->title}"
+	// Shouldn't happen if we only have images - but check anyway
+	if (imp == null)
+		continue
+	imp = preProcessing(imp, saturated, downsample)
+	path = new File(outDir, "${->title}.tif").getAbsolutePath()
+	ij.IJ.save(imp, path)
+}
